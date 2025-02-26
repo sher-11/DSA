eafbe39acebc8b302a04ed8afd3059758784c943
Shreya Chandra <31302491+sher-11@users.noreply.github.com>
2025-02-26 17:00:01 +0530

    two fibonacci soltions

--- fibonacci_series.js
+++ fibonacci_series.js
@@ -1,23 +1,24 @@
+//Solution 1:
 //Certain number of characters
-// const printFibonacciSeries = (end) => {
-//     let firstTerm = 0;
-//     let secondTerm = 1;
-//     let nextTerm;
+const printFibonacciSeries = (end) => {
+    let firstTerm = 0;
+    let secondTerm = 1;
+    let nextTerm;
 
     
-//     for (let i = 0; i < end; i++) {
-//         console.log(firstTerm);
-//         nextTerm = firstTerm + secondTerm;
-//         firstTerm = secondTerm;
-//         secondTerm = nextTerm;
+    for (let i = 0; i < end; i++) {
+        console.log(firstTerm);
+        nextTerm = firstTerm + secondTerm;
+        firstTerm = secondTerm;
+        secondTerm = nextTerm;
 
-//     }
-// }
+    }
+}
 
-// console.log(printFibonacciSeries(5));
-// console.log(printFibonacciSeries(15));
+console.log(printFibonacciSeries(5));
+console.log(printFibonacciSeries(15));
 
-
+//Solution 2:
 //Up to certain number
 const printFibonacciSeriesUpTo = (number) => {
     let firstTerm = 0;
